5759da8ae6c2041db20830d53925996781001c87
Jaber Ahmed <Jaber.a259@hotmail.com>
2024-10-19 20:55:21 +0100

    tidy classes and code, remove date selection and add rover image api

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 const baseUrl = 'https://api.nasa.gov/planetary/apod?api_key=';
-// import {apiKey} from 'apiKey.js';
 const apiAccess = new ApiKey();
-//HIDE YOUR DAMN API KEY, WE CANNOT COMMIT IF WE CANNOT HIDE IT
 const apiKey = apiAccess.apiKey;
 
 //Selecting our HTML elements
 const title = document.querySelector('#title');
+const date = document.querySelector('#date');
 const copyright = document.querySelector('#copyright');
 const mediaSection = document.querySelector('#media-section');
 const information = document.querySelector('#description');
-const dateInput = document.querySelector('#datepicker')
 
-//experimenting
-let date = "&date=" + "date(yyyy-mm-dd)" + "&";
-//create new object of the current date and ISOS is the date format and we are slicing the string upto 10 characters
-const currentdate = new Date().toISOString().slice(0, 10);
-//setting min max, first photo published was on the 1995-06-16
-dateInput.max = currentdate;
-dateInput.min = "1995-06-16";
-
+const apod = document.querySelector("#image");
+// const apod = document.getElementById("#image");
 
 //fetch api data
 function fetchData() {
@@ -26,33 +18,30 @@ function fetchData() {
         fetch(baseUrl + apiKey).then(Response => Response.json())
             .then(json => {
                 displayTitle(json);
+                displayDate(json);
                 displayCopyright(json);
                 displayMedia(json);
                 displaydata(json);
-
+                console.log(json);
+                //console.log(json.date);
             })
     } catch (error) {
         console.log(error);
     }
 }
 
-//adding containers to hold our media temporarily
-const imageSection =
-    `<a id="hdimg" href="" target="-blank">
-     <div class="">
-     <img id="image_of_the_day" src="" alt="image-by-nasa">
-     </div>
-     </a>`
 
-const videoSection =
-    `<div class="video-div">
-     <iframe id="videoLink" src="" frameborder="0"></iframe>
-     </div>`
 
 //display functions
 function displayTitle(data) {
     title.innerHTML = data.title;
 }
+
+function displayDate(data) {
+    
+    date.innerHTML = data.date;
+}
+
 
 function displayCopyright(data) {
     if (data.hasOwnProperty('copyright')) {
@@ -62,21 +51,42 @@ function displayCopyright(data) {
     }
 }
 
+
 function displayMedia(data) {
-    // not sure what the syntax here is doing?
-    if (data.media_type == "video") {
-        mediaSection.innerHTML = videoSection;
-        document.getElementById("videoLink").src = data.url;
-    } 
-    
-        mediaSection.innerHTML = imageSection;
-        document.getElementById("hdimg").href = data.hdurl;
-        document.getElementById("image_of_the_day").src = data.url;
+
+    //testing variable
+     const media = "video";
+
+    try {
+        if (data.media_type == "video") {
+        // if (media == "video"){
+             // if 
+            const videoSection =
+            `<div class="video-div">
+            <iframe id="video" src="" frameborder="0"></iframe>
+            </div>`
+            //place iframe into container in html
+            mediaSection.innerHTML = videoSection;
+            
+            ////placing APOD json url into video source
+            document.getElementById("video").src = "https://www.youtube.com/embed/bsH33w3Qa6M";
+        }
+        //placing APOD json url into image source
+            apod.src = data.url;
+    }catch {
+
+        console.log("ERROR!Cannot display any text")
+
+    }
+
+
 }
 
 function displaydata(data) {
     information.innerHTML = data.explanation;
 }
+
+
 
 //calling methods
 fetchData();
